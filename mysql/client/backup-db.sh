e425360070f0b91bfcd7e46d3ee063153a39f3df
Craig Barr <craig.barr@rubiconred.com>
2019-04-19 06:40:13 +1000

    switched to make the function a bit more reusable

--- mysql/client/backup-db.sh
+++ mysql/client/backup-db.sh
@@ -7,10 +7,8 @@
 backupDatabase(){
   [[ ! -z "${IMAGE}" ]] || IMAGE=mysql:5.6
   DATETIME=`date +%Y-%m-%d-%H-%M-%S`
-  FILENAME=${4}-${DATETIME}.sql
+  FILENAME=${1}-${DATETIME}.sql
 docker run --rm -ti ${IMAGE} /bin/sh -c \
-"mysqldump -u ${1} -p${2} -h ${3} --databases ${4}" > ${FILENAME}
+"mysqldump -u ${USERNAME} -p${PASSWORD} -h ${HOST} --databases ${1}" > ${FILENAME}
+  [[ -z "${S3_BUCKET}" ]] || aws s3 cp ${FILENAME} ${S3_BUCKET}/ ${S3_ARGS}
 }
-
-backupDatabase ${USERNAME} ${PASSWORD} ${HOST} ${DATABASE}
-[[ -z "${S3_BUCKET}" ]] || aws s3 cp ${FILENAME} ${S3_BUCKET} ${S3_ARGS}
